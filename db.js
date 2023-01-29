b461fc2603b60bb54a6678baf7b43399392c0f79
sycrw <=>
2023-01-29 22:27:44 +0100

    started rock paper scissors

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ Creating database connection over the my mysql2 module.
 */
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-  no,
+  pw: "not" + "for" + "you" + "mate",
 });
 /*
 IMPORTANT! crashes backend if DB connection cant be established
